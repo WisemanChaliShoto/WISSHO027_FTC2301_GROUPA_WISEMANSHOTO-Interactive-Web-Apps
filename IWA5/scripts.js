a4da5b4acef19e0c12d6fd1a93b099260799a7cc
Charlie Wiseman Shoto <charlie.shoto@gmail.com>
2023-04-04 14:53:15 +0200

    last commit

--- IWA5/scripts.js
+++ IWA5/scripts.js
@@ -33,40 +33,75 @@ customers = '1'
 const location = 'RSA'
 currency = null */
 
-FREE_WARNING = 'Free shipping only applies to single customer orders'
-BANNED_WARNIN = 'Unfortunately we do not ship to your country of residence'
-NONE_SELECTED = 0
+const FREE_WARNING = 'Free shipping only applies to single customer orders'
+const BANNED_WARNING = 'Unfortunately we do not ship to your country of residence'
+const NONE_SELECTED = 0
 
-if (location === RSA) { shipping === 400 && currency === 'R' }
+let RSA;
 
-if (location === NAM) {
+let NAM;
 
-    shipping = 600 }
-else {
- shipping = 800
+const location = 'RSA';
+
+let customer = 1;
+
+let shipping = null;
+
+let  currency = null;
+
+if (location==='RSA') {
+	shipping === 400 && currency === 'R' ;
+	
+	
+} 
+else if (location===NAM) {
+	shipping === 600 && currency === '$'
+	
 }
 
-shoes = 300 * 1
-toys - 100 * 5
-shirts = 150 * 'NONE_SELECTED'
-batter 35 * 2
-pens = 5 * 'NONE_SELECTED' 
+else if (location !== RSA || location !== NAM) {
 
-shipping = null
-currency = $
+	shipping===800 && currency ==='$';
 
-if (shoes + batteries + pens + shirts > 1000 &&  ) {
-	if (location = NAM && customers < 2) {
-			if (location = RSA)
-		    shipping = 0 || calcShipping
-		}
+} 
+
+
+	const shoes = 300 * 1;
+	const toys = 100 * 5;
+	const shirts = 150 * NONE_SELECTED
+	const batteries = 35 * 2
+	const pens = 5 * NONE_SELECTED
+
+if (location === RSA && shoes + batteries + pens + shirts >= 1000) {
+
+	shipping === 0 && customer === 1;
+	currency === 'R';
+}
+if (location === NAM && shoes + batteries + pens + shirts >= 60) {
+
+	shipping === 0 && customer === 1;
+	currency === '$';
+	
+}
+
+
+
+	if (location=== 'RSA') {
+
+		console.log('Price',+ currency + shipping + shoes + batteries + pens + shirts);
+		
+	}
+	else if (location === 'RSA'  ) {
+
+		console.log('Price',+ currency + shipping + shoes + batteries + pens + shirts);
+		
 	}
 
+	else if ( location === 'RSA' || 'NAM' && customer !== 1 )
+	console.log(FREE_WARNING);
+	
+	else if (location === 'NK') 
 
-if (shipping == 0 && customers !== 1) { console.log(WARNING) }
+	console.log(BANNED_WARNING);
 
-location = 'NK' ? console.log(WARNING) : console.log('price', currency, shoes + batteries + pens + shirts + shipping)
-
-customers = '1'
-const location = 'RSA'
-currency = null
+	
