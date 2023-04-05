b8f892447ed0ff6c34b00cafb6d30c0dc37ba467
Charlie Wiseman Shoto <charlie.shoto@gmail.com>
2023-04-05 14:09:17 +0200

    IWA6 Commit

---
+++ IWA6/Challenge 1/script.js
@@ -0,0 +1,24 @@
+/*const primaryPhone = 'O748105141'
+const secondaryPhone = '0219131568'
+
+// Only change below this line
+secondaryPhone === typeof number;
+
+const primaryValid = typeof number == primaryPhone 
+const secondaryValid = typeof number == secondaryPhone
+
+
+console.log('Primary phone is valid numerical string:', primaryValid)
+console.log('secondary phone is valid numerical string:', secondaryValid )
+*/
+
+const primaryPhone = 'O748105141'
+const secondaryPhone = '0219131568'
+
+// Only change below this line
+
+const primaryValid = isNAN(primaryPhone) === false
+const secondaryValid = isNAN(secondaryPhone) === false
+
+console.log('Primary phone is valid numerical string:', primaryValid)
+console.log('Secondary phone is valid numerical string:', secondaryValid)
