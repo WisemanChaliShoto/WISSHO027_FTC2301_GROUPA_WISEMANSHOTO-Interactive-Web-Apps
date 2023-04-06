ad2b59a4c783fbd30d314b1412d60b37545af816
Charlie Wiseman Shoto <charlie.shoto@gmail.com>
2023-04-06 13:49:24 +0200

    IWA7

---
+++ IWA7/Challenge2/script.js
@@ -0,0 +1,11 @@
+const nickName = "Timmy";
+const firstName = "Timothy";
+
+
+if (nickName) {
+  console.log(`Good morning, ${nickName}!`);
+} else if (firstName) {
+  console.log(`Good morning, ${firstName}!`);
+} else {
+  console.log("Good morning!");
+}
